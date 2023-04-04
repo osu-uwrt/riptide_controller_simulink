5d9b9218fa476bdacc95876ab20844e435572eff
hunter <hunterseachrist@gmail.com>
2023-04-04 10:45:11 -0400

    Change test controller to just a gain

--- Porportional_Follower_Test_ert_shrlib_rtw/Porportional_Follower_Test.c
+++ Porportional_Follower_Test_ert_shrlib_rtw/Porportional_Follower_Test.c
@@ -7,9 +7,9 @@
  *
  * Code generated for Simulink model 'Porportional_Follower_Test'.
  *
- * Model version                  : 1.6
+ * Model version                  : 1.7
  * Simulink Coder version         : 9.8 (R2022b) 13-May-2022
- * C/C++ source code generated on : Mon Mar 27 19:02:21 2023
+ * C/C++ source code generated on : Mon Apr  3 12:52:26 2023
  *
  * Target selection: ert_shrlib.tlc
  * Embedded hardware selection: Intel->x86-64 (Windows64)
@@ -20,14 +20,6 @@
  */
 
 #include "Porportional_Follower_Test.h"
-#include "rtwtypes.h"
-#include "Porportional_Follower_Test_private.h"
-
-/* Block signals (default storage) */
-B_Porportional_Follower_Test_T Porportional_Follower_Test_B;
-
-/* Block states (default storage) */
-DW_Porportional_Follower_Test_T Porportional_Follower_Test_DW;
 
 /* External inputs (root inport signals with default storage) */
 ExtU_Porportional_Follower_Te_T Porportional_Follower_Test_U;
@@ -43,103 +35,21 @@ RT_MODEL_Porportional_Followe_T *const Porportional_Follower_Test_M =
 /* Model step function */
 void Porportional_Follower_Test_step(void)
 {
-  real_T rtb_Subtract_f;
-
   /* Outputs for Atomic SubSystem: '<Root>/Porportional_Follower_Test' */
-  /* Clock: '<S2>/Clock' */
-  Porportional_Follower_Test_B.Clock = Porportional_Follower_Test_M->Timing.t[0];
-
-  /* Sum: '<S2>/Subtract' incorporates:
-   *  Memory: '<S2>/Memory2'
-   */
-  rtb_Subtract_f = Porportional_Follower_Test_B.Clock -
-    Porportional_Follower_Test_DW.Memory2_PreviousInput;
-
-  /* Sum: '<S1>/Add' incorporates:
-   *  Constant: '<S1>/P'
+  /* Outport: '<Root>/Follower' incorporates:
+   *  Gain: '<S1>/Gain'
    *  Inport: '<Root>/Signal'
-   *  Memory: '<S1>/Previous Velocity'
-   *  Product: '<S1>/Acceleration'
-   *  Product: '<S1>/Delta Velocity'
-   *  Sum: '<S1>/Subtract'
    */
-  Porportional_Follower_Test_B.Add = (Porportional_Follower_Test_U.Signal -
-    Porportional_Follower_Test_DW.PreviousSignalValue_PreviousInp) * 3.0 *
-    rtb_Subtract_f +
-    Porportional_Follower_Test_DW.PreviousVelocity_PreviousInput;
-
-  /* Sum: '<S1>/Add1' incorporates:
-   *  Product: '<S1>/Delta Position'
-   */
-  Porportional_Follower_Test_B.Add1 = rtb_Subtract_f *
-    Porportional_Follower_Test_B.Add +
-    Porportional_Follower_Test_DW.PreviousSignalValue_PreviousInp;
+  Porportional_Follower_Test_Y.Follower = 5.0 *
+    Porportional_Follower_Test_U.Signal;
 
   /* End of Outputs for SubSystem: '<Root>/Porportional_Follower_Test' */
-
-  /* Outport: '<Root>/Follower' */
-  Porportional_Follower_Test_Y.Follower = Porportional_Follower_Test_B.Add1;
-
-  /* Update for Atomic SubSystem: '<Root>/Porportional_Follower_Test' */
-  /* Update for Memory: '<S1>/Previous Signal Value' */
-  Porportional_Follower_Test_DW.PreviousSignalValue_PreviousInp =
-    Porportional_Follower_Test_B.Add1;
-
-  /* Update for Memory: '<S2>/Memory2' */
-  Porportional_Follower_Test_DW.Memory2_PreviousInput =
-    Porportional_Follower_Test_B.Clock;
-
-  /* Update for Memory: '<S1>/Previous Velocity' */
-  Porportional_Follower_Test_DW.PreviousVelocity_PreviousInput =
-    Porportional_Follower_Test_B.Add;
-
-  /* End of Update for SubSystem: '<Root>/Porportional_Follower_Test' */
-
-  /* Update absolute time for base rate */
-  /* The "clockTick0" counts the number of times the code of this task has
-   * been executed. The absolute time is the multiplication of "clockTick0"
-   * and "Timing.stepSize0". Size of "clockTick0" ensures timer will not
-   * overflow during the application lifespan selected.
-   */
-  Porportional_Follower_Test_M->Timing.t[0] =
-    ((time_T)(++Porportional_Follower_Test_M->Timing.clockTick0)) *
-    Porportional_Follower_Test_M->Timing.stepSize0;
-
-  {
-    /* Update absolute timer for sample time: [0.1s, 0.0s] */
-    /* The "clockTick1" counts the number of times the code of this task has
-     * been executed. The resolution of this integer timer is 0.1, which is the step size
-     * of the task. Size of "clockTick1" ensures timer will not overflow during the
-     * application lifespan selected.
-     */
-    Porportional_Follower_Test_M->Timing.clockTick1++;
-  }
 }
 
 /* Model initialize function */
 void Porportional_Follower_Test_initialize(void)
 {
-  /* Registration code */
-  {
-    /* Setup solver object */
-    rtsiSetSimTimeStepPtr(&Porportional_Follower_Test_M->solverInfo,
-                          &Porportional_Follower_Test_M->Timing.simTimeStep);
-    rtsiSetTPtr(&Porportional_Follower_Test_M->solverInfo, &rtmGetTPtr
-                (Porportional_Follower_Test_M));
-    rtsiSetStepSizePtr(&Porportional_Follower_Test_M->solverInfo,
-                       &Porportional_Follower_Test_M->Timing.stepSize0);
-    rtsiSetErrorStatusPtr(&Porportional_Follower_Test_M->solverInfo,
-                          (&rtmGetErrorStatus(Porportional_Follower_Test_M)));
-    rtsiSetRTModelPtr(&Porportional_Follower_Test_M->solverInfo,
-                      Porportional_Follower_Test_M);
-  }
-
-  rtsiSetSimTimeStep(&Porportional_Follower_Test_M->solverInfo, MAJOR_TIME_STEP);
-  rtsiSetSolverName(&Porportional_Follower_Test_M->solverInfo,
-                    "FixedStepDiscrete");
-  rtmSetTPtr(Porportional_Follower_Test_M,
-             &Porportional_Follower_Test_M->Timing.tArray[0]);
-  Porportional_Follower_Test_M->Timing.stepSize0 = 0.1;
+  /* (no initialization code required) */
 }
 
 /* Model terminate function */
